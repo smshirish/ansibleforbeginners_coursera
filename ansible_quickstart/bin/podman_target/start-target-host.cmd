37f766f584c07eabb8f08d5eb9f5781a491cf326
smshirish <shirishsakhare@gmail.com>
2026-04-05 12:59:44 +0000

    Update podman launch port forwarding to specify listen on all interfaces

--- ansible_quickstart/bin/podman_target/start-target-host.cmd
+++ ansible_quickstart/bin/podman_target/start-target-host.cmd
@@ -1,3 +1,6 @@
 ## This starts the podman container that is to be used as target of ansible deployments
 
-podman run  -v C:\data\workspace\devops_infra\share_mounts:/shared -p 2222:22  -it my-ansible-host1
+podman run  -v C:\data\workspace\devops_infra\share_mounts:/shared -p 0.0.0.0:2222:22  -it my-ansible-host1
+
+## building podman image with the following command:
+podman build -f C:\data\workspace\devops_infra\ansible\ansibleforbeginners_coursera\ansible_quickstart\docker_images\Dockerfile-ansible-target1 . -t my-ansible-host1:latest
