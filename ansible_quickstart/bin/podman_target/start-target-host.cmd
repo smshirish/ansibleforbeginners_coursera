80ef0733b4932cc06a6201189b64d3c127566cb4
smshirish <shirishsakhare@gmail.com>
2026-04-06 08:46:50 +0000

    added comments

--- ansible_quickstart/bin/podman_target/start-target-host.cmd
+++ ansible_quickstart/bin/podman_target/start-target-host.cmd
@@ -1,5 +1,6 @@
 ## This starts the podman container that is to be used as target of ansible deployments
-
+## -v share volume to share files between host and container, -p to map port 22 of container to 2222 on host machine, -it for interactive mode and my-ansible-host1 is the name of the podman image to be used for launching the container.
+## -p 0.0.0.0 makes the port accessible from any network interface on the host machine, which is necessary for Ansible to connect to the container as a target host.
 podman run  -v C:\data\workspace\devops_infra\share_mounts:/shared -p 0.0.0.0:2222:22  -it my-ansible-host1
 
 ## building podman image with the following command:
