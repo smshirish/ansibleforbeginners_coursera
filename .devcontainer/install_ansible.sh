89189fac86036f6e8344cc4b0f505d4e4265fff0
smshirish <shirishsakhare@gmail.com>
2026-04-12 15:30:51 +0000

    removed duplicate dev container files

--- .devcontainer/install_ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-apt-get update && apt-get install -y pipx
-pipx ensurepath --global
-
-# Install Ansible and Lint globally so they are in /usr/local/bin
-PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install --include-deps ansible
-PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install ansible-lint
